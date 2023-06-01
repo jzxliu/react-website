ac05f224b269181fcfdfc301da790bb4be5138e4
jackzxliu <jacks.liu@mail.utoronto.ca>
2023-06-01 13:13:21 -0400

    added homepage thing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,33 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
+import './Navbar.css';
+import {Button} from "./Button";
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(()=>{showButton()}, []);
+
+    window.addEventListener('resize', showButton);
 
     return (
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    <Link to='/' className='navbar-logo'>
-                        TRVL <i className="fab fa-typo3"/>
+                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                        Jack's RAM &nbsp; <i className="fa-solid fa-shield-cat"/>
                     </Link>
                 </div>
                 <div className='menu-icon' onClick = {handleClick}>
@@ -25,12 +41,12 @@ function Navbar() {
                     </li>
                     <li className="nav-item">
                         <Link to='/services' className='nav-links' onClick={closeMobileMenu} >
-                            Services
+                            Blog
                         </Link>
                     </li>
                     <li className="nav-item">
                         <Link to='/products' className='nav-links' onClick={closeMobileMenu} >
-                            Products
+                            Projects
                         </Link>
                     </li>
                     <li className="nav-item">
@@ -39,6 +55,7 @@ function Navbar() {
                         </Link>
                     </li>
                 </ul>
+                {button && <Button buttonStyle='btn-outline'>SIGN UP</Button> }
             </nav>
         </>
     );
