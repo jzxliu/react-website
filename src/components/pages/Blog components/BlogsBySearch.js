d621c70eadd46123e50ceeb1846d7a16ace6af96
jackzxliu <jacks.liu@mail.utoronto.ca>
2023-06-15 12:33:29 -0400

    fixed some warnings

--- src/components/pages/Blog components/BlogsBySearch.js
+++ src/components/pages/Blog components/BlogsBySearch.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react';
 import {useParams} from "react-router-dom";
 import {request} from "graphql-request";
 import Posts from "./Posts";
-import Search from "../../Search";
 
 const BlogsBySearch = () => {
     const {slug} = useParams()
