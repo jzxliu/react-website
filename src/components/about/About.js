71dc78dd4b25658039292f2e1e429042979321d5
jackzxliu <jacks.liu@mail.utoronto.ca>
2023-06-23 18:31:31 -0700

    new page!

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './About.css';
 
 function About() {
     return (
-        <div>
-            About Page
+        <div className='about-container'>
+            <h1>About this site</h1>
+            <p>
+                This site is developed using React.js <br/>
+                Using GraphQL as database <br/>
+                and Hosted using Netlify
+            </p>
         </div>
     );
 }
