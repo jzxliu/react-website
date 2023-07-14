d0d72c21ad9695d5b736258e729bf10b5411a511
jackzxliu <jacks.liu@mail.utoronto.ca>
2023-07-14 17:23:23 -0400

    simplified

--- src/graphql/Queries.js
+++ src/graphql/Queries.js
@@ -1,18 +1,6 @@
 import { gql } from "graphql-request";
 
-export const QUERY_URL = 'https://ca-central-1.cdn.hygraph.com/content/clin9ia78079n01ugb1jj7d3z/master'
-export const QUERY_SLUG_CATEGORIES = gql`
-{
-    categories {
-      name
-      slug
-    }
-}
-`
-
-export const QUERY_BLOGS = gql`
-{
-  posts(orderBy: datePublished_DESC){
+const post = `
     id
     title
     datePublished
@@ -27,6 +15,22 @@ export const QUERY_BLOGS = gql`
       color {css}
     }
     description
+`
+
+export const QUERY_URL = 'https://ca-central-1.cdn.hygraph.com/content/clin9ia78079n01ugb1jj7d3z/master'
+export const QUERY_SLUG_CATEGORIES = gql`
+{
+    categories {
+      name
+      slug
+    }
+}
+`
+
+export const QUERY_BLOGS = gql`
+{
+  posts(orderBy: datePublished_DESC){
+    ${post}
   }
   
   countConnection: postsConnection{
@@ -40,20 +44,7 @@ export const QUERY_BLOGS = gql`
 export const QUERY_BY_CATEGORY = gql`
   query GetPostsByCategory($slug: String!) {
       posts(where: {categories_some: {slug: $slug}}){
-        id
-        title
-        datePublished
-        slug
-        coverPhoto {
-          url
-        }
-        categories {
-          name
-          id
-          slug
-          color {css}
-        }
-        description
+        ${post}
       }
 }
 `
@@ -61,27 +52,14 @@ export const QUERY_BY_CATEGORY = gql`
 export const QUERY_BY_SEARCH = gql`
   query GetPostsByCategory($slug: String!) {
       posts(where: {_search: $slug}){
-        id
-        title
-        datePublished
-        slug
-        coverPhoto {
-          url
-        }
-        categories {
-          name
-          id
-          slug
-          color {css}
-        }
-        description
+        ${post}
       }
 }
 `
 
 export const QUERY_POST = gql`
-  query GetPost($slug: String!){
-  posts(where: {_search: $slug}){
+query GetPost($slug: String!){
+  posts(where: {slug: $slug}){
     id
     title
     datePublished
