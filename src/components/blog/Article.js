f879e164571c16d0150fdcc511ae0b28b6c47257
jackzxliu <jacks.liu@mail.utoronto.ca>
2023-07-18 17:28:33 -0400

    added scrolling to articles

--- src/components/blog/Article.js
+++ src/components/blog/Article.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import "./Article.css"
 import {QUERY_POST} from "../../graphql/Queries";
 import useQueryPosts from "../../graphql/useQueryPosts";
 
 const Article = () => {
+
+
     const {posts, error} = useQueryPosts({query: QUERY_POST});
+
+    const ref = useRef(null);
+
+    const executeScroll = () => ref.current.scrollIntoView({behavior: 'smooth'})
+
     if (error) return <h2>{error}</h2>
     return (
         <div className='article-container'>
             {
                 posts?.map(posts =>(
                     <React.Fragment key={posts.id}>
-                        <article>
+                        <article onLoad={executeScroll}>
                             <div className="cover-container">
                                 <img src={posts.coverPhoto.url} alt=""/>
                             </div>
+                            <div className='article-ref-div' ref={ref}></div>
                             <h2 className="title">{posts.title}</h2>
                             <div className="published-time">
                                 <i className="far fa-clock"></i>
