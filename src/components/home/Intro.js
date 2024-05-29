1d98153af310d3bda45c411335850eafc2412f31
jackzxliu <jacks.liu@mail.utoronto.ca>
2024-05-28 23:24:18 -0400

    aaa

--- src/components/home/Intro.js
+++ src/components/home/Intro.js
@@ -9,12 +9,9 @@ function Intro(){
                 <h1> Hello! My name is Jay.</h1>
                 <text>
                     <br/>
-                    I'm a second year Computer-Science specialist at the University of Toronto. <br/>
-                    Currently learning frontend development and UX design, while planning to focus in AI in the future. <br/>
-                    This year I wanted to push new limits by constantly setting goals and challenges for myself,
-                    striving for a fitter, happier and more productive lifestyle. <br/>
-                    I have decided to make a blog to keep track of this journey of self discovery,
-                    and to document everything I'm passionate about. Enjoy!<br/>
+                    I'm a soon-to-be fourth year Computer-Science specialist at the University of Toronto. <br/>
+                    As an intern at Amazon, I enjoy having an active lifestyle and always pushing myself to new limits. <br/>
+                    This is a blog to keep track of this journey of self discovery, and to document everything I'm passionate about. Enjoy!<br/>
                 </text>
             </div>
             <img className='img' alt='hello' src={'images/hello.jpg'}/>
